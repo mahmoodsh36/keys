e70286cc350c1f3889afaaa37ac726529093679e
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-04-03 11:24:03 +0300

    "properly" run independent commands/programs

--- utils.py
+++ utils.py
@@ -1,14 +1,33 @@
 import subprocess
-
+import os
 from os import listdir
 from os.path import join
 
 def run_cmd(cmd):
-    subprocess.Popen(
-        f"su mahmooz -c 'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus WAYLAND_DISPLAY=wayland-1 GDK_BACKEND=wayland XDG_RUNTIME_DIR=/run/user/1000 {cmd}'",
-        shell=True,
-        start_new_session=True
-    )
+    username = 'mahmooz'
+
+    old_env = os.environ.copy()
+    env = {}
+    defaults = {
+        "DBUS_SESSION_BUS_ADDRESS": "unix:path=/run/user/1000/bus",
+        "WAYLAND_DISPLAY": "wayland-1",
+        "GDK_BACKEND": "wayland",
+        "XDG_RUNTIME_DIR": "/run/user/1000",
+        "PATH": old_env['PATH']
+    }
+    for key, value in defaults.items():
+        env.setdefault(key, value)
+
+    with open(os.devnull, 'wb') as dn:
+        subprocess.Popen(
+            cmd,
+            # stdout=dn, stderr=dn, stdin=dn,
+            start_new_session=True,
+            # close_fds=True,
+            env=env,
+            shell=True,
+            user='mahmooz',
+        )
 
 def mod(key):
     """use key as modifier"""
